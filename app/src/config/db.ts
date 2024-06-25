043e93a74455b7f7392048c7c896a9c738e2062c
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-06-25 18:48:10 +0300

    Db init

--- app/src/config/db.ts
+++ app/src/config/db.ts
@@ -1,78 +1,15 @@
-import { Client, createClient } from "@libsql/client";
-import { Quote } from "./types";
+import { Client, createClient } from '@libsql/client';
 
-
-
-class BaseEntity<T> {
-    protected client: Client;
-    protected tableName: string;
-
-    constructor(client: Client, tableName: string) {
-        this.client = client;
-        this.tableName = tableName;
-    }
-    
-    async create(item: T): Promise<boolean> {
-        // // Implementation for creating an item
-        // const transaction = await this.client.transaction("write")
-        // const res = await transaction.execute(`INSERT INTO ${this.tableName} VALUES (${item})`);
-        // transaction.commit();
-        // return true;        
-        throw new Error("NOT IMPLEMENTED")
-
-    }
-
-    async read(id: string): Promise<T | null> {
-        // Implementation for reading an item by ID
-        const result = await this.client.execute(`SELECT * FROM ${this.tableName} WHERE id = '${id}'`);
-        return result.toJSON()[0] || null;
-    }
-
-    async update(id: string, item: T): Promise<void> {
-        // // Implementation for updating an item by ID
-        // const res = await this.client.execute(`UPDATE ${this.tableName} SET ? WHERE id = '${id}'`, [item]);
-        throw new Error("NOT IMPLEMENTED")
-    }
-
-    async delete(id: string): Promise<void> {
-        // // Implementation for deleting an item by ID
-        // await this.client.execute(`DELETE FROM ${this.tableName} WHERE id = '${id}'`);
-        throw new Error("NOT IMPLEMENTED")
-    
-    }
+if (!process.env.DATABASE_URL) {
+    throw new Error("No DATABASE_URL variable found in environment");
 }
-class QuoteEntity extends BaseEntity<Quote> {
-    constructor(client: Client) {
-        super(client, 'quotes');
-    }
+const config = {
+    url: process.env.DATABASE_URL,
+    encryptionKey: process.env.DATABASE_AUTH_TOKEN,
+};
 
-    // Additional methods specific to Quote entity can be added here
+export const client = createClient(config);
+
+export const initializeDb = async () => {
+    client.execute("CREATE TABLE IF NOT EXISTS quotes(id TEXT primary key, quote TEXT NOT NULL, birth_year INTEGER, death_year INTEGER, author TEXT)")
 }
-
-
-
-
-class DbContextConfigurator {
-    protected client: Client;
-
-    constructor() {
-        if (!process.env.DATABASE_URL) {
-            throw new Error("No DATABASE_URL variable found in environment");
-        }
-        const config = {
-            url: process.env.DATABASE_URL,
-            encryptionKey: process.env.DATABASE_AUTH_TOKEN,
-        };
-        this.client = createClient(config);
-    }
-}
-
-class DbContext extends DbContextConfigurator {
-    public quote: QuoteEntity;
-
-    constructor() {
-        super();
-        this.quote = new QuoteEntity(this.client);
-    }
-}
-
--- app/src/index.ts
+++ app/src/index.ts
@@ -6,18 +6,18 @@ import { html } from "@elysiajs/html";
 // Controllers
 import homeController from "./controllers/homeController";
 import staticPlugin from "@elysiajs/static";
-// import TODO: Authentication Controller
-
-
 
 const app = new Elysia();
 
 app.use(html());
 app.use(staticPlugin());
-
 app.use(homeController);
-    
+
+
+
 app.listen(3000);
+
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
