e3281dd821959b6b30602f91f4f3e94395a7d39c
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-07-02 17:15:04 +0300

    migrations work

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   schema: './db/quotes.ts',
   out: './db/migrations',
+  driver: "turso",
   dialect: 'sqlite', // 'postgresql' | 'mysql' | 'sqlite'
   dbCredentials: {
     url: process.env.DATABASE_URL,
     authToken: process.env.DATABASE_AUTH_TOKEN,
-    token: process.env.DATABASE_AUTH_TOKEN
   },
 });
